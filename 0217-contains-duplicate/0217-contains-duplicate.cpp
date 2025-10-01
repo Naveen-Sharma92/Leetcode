8bb37c7d50c6b18a4ffc1d72bc942864aad968b1
Naveen Sharma <naveensharma9267@gmail.com>
2025-10-01 19:41:26 +0530

    Time: 58 ms (28%), Space: 90.9 MB (45.76%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,29 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int,int>m;
+//         for(int i=0;i<n;i++){
+//             m[nums[i]]++;
+//         }
+//         for(auto it:m){
+//             if(it.second>1)
+//             return true;
+//         }
+//         return false;
+
+//     }
+// };
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int,int>m;
         for(int i=0;i<n;i++){
+            
+            if(m.count(nums[i])>0)
+                return true;
             m[nums[i]]++;
-        }
-        for(auto it:m){
-            if(it.second>1)
-            return true;
         }
         return false;
 
