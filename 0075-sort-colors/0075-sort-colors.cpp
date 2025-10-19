5d741ecc24d667d533f726c6470263893ffaabf8
Naveen Sharma <naveensharma9267@gmail.com>
2025-10-19 21:18:42 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,28 +1,50 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         //dutch nationla flag algorithm
+//         int low=0,mid =0,n=nums.size(),i=0;
+//         int high = n-1;
+//         while(i<n){
+//             if(nums[i]==0){
+//                 low++;
+//                 mid++;
+//             }
+//             else if(nums[i]==1)
+//                 mid++;
+//             else
+//                 high--;
+//             i++;
+//         }
+//         for(int i=0;i<n;i++){
+//             if(i<low)
+//             nums[i]=0;
+//             else if(i<mid)
+//             nums[i]=1;
+//             else
+//             nums[i]=2;
+//             cout<<nums[i]<<",";
+//         }
+
+//     }
+// };
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         //dutch nationla flag algorithm
-        int low=0,mid =0,n=nums.size(),i=0;
-        int high = n-1;
-        while(i<n){
-            if(nums[i]==0){
+        int low=0,mid =0;
+        int high = nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
                 low++;
                 mid++;
             }
-            else if(nums[i]==1)
+            else if(nums[mid]==1)
                 mid++;
-            else
+            else{
+                swap(nums[mid],nums[high]);
                 high--;
-            i++;
-        }
-        for(int i=0;i<n;i++){
-            if(i<low)
-            nums[i]=0;
-            else if(i<mid)
-            nums[i]=1;
-            else
-            nums[i]=2;
-            cout<<nums[i]<<",";
+            }
         }
 
     }
