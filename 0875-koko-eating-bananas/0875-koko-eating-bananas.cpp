2df14ee619141fd851dc6274e7bca094cdc18356
Naveen Sharma <naveensharma9267@gmail.com>
2025-12-22 18:40:02 +0530

    Time: 9 ms (48.55%), Space: 22.9 MB (76.23%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,7 +3,6 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int n=piles.size();
         int low=1,high= *max_element(piles.begin(), piles.end());
-        int ans=high;
         while(low<=high){
             int mid=low+(high-low)/2;
             long long total=0;
@@ -14,10 +13,9 @@ public:
                 low=mid+1;
             }
             else {
-                ans=mid;
                 high=mid-1;
             }
         }
-    return ans;
+    return low;
     }
 };
