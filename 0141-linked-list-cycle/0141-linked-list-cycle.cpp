c84a1af7be1113825bc1e6996dcc49cd2823d84c
Naveen Sharma <naveensharma9267@gmail.com>
2026-01-05 22:24:11 +0530

    Time: 6 ms (90.51%), Space: 11.9 MB (21.79%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,7 +11,7 @@ public:
     bool hasCycle(ListNode *head) {
         if(head==NULL)
             return false;
-        ListNode *slow=head,*fast=head,*temp=head;
+        ListNode *slow=head,*fast=head;
         while(fast->next!=NULL && fast->next->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
