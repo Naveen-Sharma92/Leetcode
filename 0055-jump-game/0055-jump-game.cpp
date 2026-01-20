114200568fae103db54800471ccb6507fb3dc02c
Naveen Sharma <naveensharma9267@gmail.com>
2026-01-20 20:44:21 +0530

    Time: 0 ms (100%), Space: 52.3 MB (35.84%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int max_jump=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>max_jump)
+            return false;
+            max_jump=max(max_jump,i+nums[i]);
+        }
+        return true;
+    }
+};
