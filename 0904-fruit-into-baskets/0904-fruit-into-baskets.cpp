dae5a1f98782e17ba7898c23232b128b75c1efd1
Naveen Sharma <naveensharma9267@gmail.com>
2026-01-27 23:38:36 +0530

    Time: 53 ms (19.21%), Space: 84.9 MB (35.68%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,29 +1,45 @@
-class Solution {
-public:
-    int totalFruit(vector<int>& fruits) {
-       int prev=fruits[0],current=fruits[0],pf=0,cf=0,ans=0,previ=0;
-       for(int i=0;i<fruits.size();i++){
-        if(fruits[i]==current){
-            cf++;
-        }
-        else if(fruits[i]== prev){
-            pf++;
-        }
-        else{
-            
-            if(fruits[i-1]== current){
-                prev = fruits[i];
-                pf=1;
+//chatgpt;
+// class Solution {
+// public:
+//     int totalFruit(vector<int>& fruits) {
+//         map<int,int >m;
+//         int ans=0,map_size=0;
+//         for(int i=0,j=0;j<fruits.size();j++){
+//             m[fruits[j]]++;
+//             if(m[fruits[j]] == 1){
+//                 map_size++;
+//             }
+//             if(map_size >2){
+//                 while(map_size>2){
+//                     m[fruits[i]]--;
+//                     if(m[fruits[i]] == 0)
+//                         map_size--;
+//                     i++;
+//                 }
+//             }
+//             ans = max(ans,j-i+1);
+//         }
+//         return ans;
+//     }
+// };
+ class Solution {
+ public:
+     int totalFruit(vector<int>& fruits) {
+        int maxlen=0,r=0,l=0,n=fruits.size();
+        unordered_map<int,int>mpp;
+        while(r<n){
+            mpp[fruits[r]]++;
+            if(mpp.size()>2){
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0)
+                    mpp.erase(fruits[l]);
+                l++;
             }
             else{
-                current = fruits[i];
-                cf=1;
+                maxlen=max(maxlen,r-l+1);
             }
-            if(fruits[previ]== current)
-                previ++;
+            r++;
         }
-        ans = max(ans,cf+pf);
-       }
-       return ans;
+        return maxlen;
     }
-};
+    };
