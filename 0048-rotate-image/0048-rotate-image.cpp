b11b86d91c1cb569ca0b2a0f541e0bd8f4aec109
Naveen Sharma <naveensharma9267@gmail.com>
2026-01-29 16:40:58 +0530

    Time: 0 ms (100%), Space: 10.2 MB (69.07%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,34 @@
 //brute
+// class Solution {
+// public:
+//     void rotate(vector<vector<int>>& matrix) {
+//         int n=matrix.size();
+//         vector<vector<int>> temp;
+//         temp.resize(n, vector<int>(n));
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++)
+//             temp[i][j]=matrix[i][j];
+//         }
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++)
+//             matrix[i][j]=temp[n-j-1][i];
+//         }
+//         return;
+//     }
+// };
+//optimal
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        vector<vector<int>> temp;
-        temp.resize(n, vector<int>(n));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++)
-            temp[i][j]=matrix[i][j];
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++)
-            matrix[i][j]=temp[n-j-1][i];
-        }
+        //transpose
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++)
+                swap(matrix[i][j],matrix[j][i]);
+        //then reverse all rows
+        for (int i = 0; i < n; i++) 
+            reverse(matrix[i].begin(), matrix[i].end());
+        
         return;
     }
 };
