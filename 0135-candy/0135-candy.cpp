bfb4e55026525700819be582c60d121bdf112e74
Naveen Sharma <naveensharma9267@gmail.com>
2026-02-03 17:07:54 +0530

    Time: 0 ms (100%), Space: 22.2 MB (77.89%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -13,25 +13,62 @@
 //     }
 // };
 
+// my approch better brute is in copy
 //we need to do double pass hint(chatgpt)
+
+
+// class Solution {
+// public:
+//     int candy(vector<int>& ratings) {
+//         int n=ratings.size();
+//         vector<int>v(n,1);
+//         int ans=0;
+//         for(int i=1;i<n;i++){
+//             if(ratings[i]>ratings[i-1]){
+//                 v[i]=v[i-1]+1;
+//             }
+//         }
+//         for(int i=n-1;i>=1;i--){
+//             if(ratings[i-1]>ratings[i]){
+//                 v[i-1]=max(v[i-1],v[i]+1);
+//             }
+//         }
+//         for(int i=0;i<n;i++)
+//             ans+=v[i];
+//         return ans;
+//     }
+// };
+
+
+//best
 class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
-        vector<int>v(n,1);
-        int ans=0;
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                v[i]=v[i-1]+1;
+        int i=1,sum=1;  // sum=1 because we are starting from index 1 not 0
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                sum+=1;
+                i++;
+            }
+            else{
+                int peak=1;
+                while(i<n && ratings[i]>ratings[i-1]){
+                    peak+=1;
+                    sum+=peak;
+                    i++;
+                }
+                int down=1;
+                while(i<n && ratings[i]<ratings[i-1]){
+                    sum+=down;
+                    down+=1;
+                    i++;
+                }
+                if(down-peak>0)
+                    sum+=(down-peak);
             }
         }
-        for(int i=n-1;i>=1;i--){
-            if(ratings[i-1]>ratings[i]){
-                v[i-1]=max(v[i-1],v[i]+1);
-            }
-        }
-        for(int i=0;i<n;i++)
-            ans+=v[i];
-        return ans;
+        return sum;
     }
 };
+
