956cf3f496dd4cd54e0da7608affcdfeec84a6be
Naveen Sharma <naveensharma9267@gmail.com>
2026-06-09 10:16:13 +0530

    Time: 29 ms (13.39%), Space: 17.2 MB (36.82%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        // by using floyd algo
+
+        vector<vector<int>>mat(n,vector<int>(n,1e8));
+        int ansCity=0,cnt=n;
+        for(auto it:edges){
+            mat[it[0]][it[1]]=it[2];
+            mat[it[1]][it[0]] = it[2];
+        }
+
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(i==j)
+                        mat[i][j]=0;
+                    else
+                        if(mat[i][k]!=1e8 && mat[k][j]!=1e8)
+                            mat[i][j]= min(mat[i][j],mat[i][k]+mat[k][j]);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){  //city
+        int c=0;
+            for(int j=0;j<n;j++){
+                if(mat[i][j] <=distanceThreshold){
+                    c++;
+                }
+            }
+            if(c<=cnt){
+                cnt=c;
+                ansCity=i;
+            }
+        }
+
+        return ansCity;
+    }
+};
