92cd578c730be56e9a2c2a1b51c82429d65ec92b
Naveen Sharma <naveensharma9267@gmail.com>
2026-06-15 18:04:16 +0530

    Time: 47 ms (11.8%), Space: 57.6 MB (7.95%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -32,21 +32,56 @@ public:
         }
     }
 };
+
+//using dsu
+// class Solution {
+// public:
+//     int makeConnected(int n, vector<vector<int>>& connections) {
+//         int a= connections.size();
+//         if(a<n-1)
+//             return -1;
+//         int c=0;
+//         vector<int>vis(n,0);
+//         DisJoint ds(n);
+//         for(auto it: connections){
+//             ds.unionbysize(it[0],it[1]);
+//         }
+//         for(int i=0;i<n;i++){
+//             if(ds.parent[i]==i)
+//                 c++;
+//         }
+//         return c-1;
+//     }
+// };
+
+//using simple traversal
 class Solution {
 public:
+void dfs(int i, vector<int>& vis,vector<vector<int>>& adj){
+    vis[i]=1;
+    for(auto it: adj[i]){
+        if(vis[it]==0)
+            dfs(it,vis,adj);
+    }
+    return;
+}
     int makeConnected(int n, vector<vector<int>>& connections) {
         int a= connections.size();
         if(a<n-1)
             return -1;
         int c=0;
         vector<int>vis(n,0);
-        DisJoint ds(n);
+        vector<vector<int>>adj(n);
         for(auto it: connections){
-            ds.unionbysize(it[0],it[1]);
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]); // this i miss so got error 
         }
+
         for(int i=0;i<n;i++){
-            if(ds.parent[i]==i)
+            if(vis[i]==0){
+                dfs(i,vis,adj);
                 c++;
+            }
         }
         return c-1;
     }
