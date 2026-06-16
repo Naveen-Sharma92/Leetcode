d8bb91a1894709fcd819f2785911fd89f0ce757e
Naveen Sharma <naveensharma9267@gmail.com>
2026-06-16 11:02:46 +0530

    Time: 15 ms (7.58%), Space: 46.9 MB (10.61%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans="";
+        for(auto it:s){
+            if(it=='*'){
+                if(ans.size()!=0)
+                    ans.pop_back();
+            }
+            else if(it=='#'){
+                string a = ans;
+                ans.append(a);
+            }
+            else if(it=='%'){
+                reverse(ans.begin(),ans.end());
+            }
+            else{
+                ans.push_back(it);
+            }
+        }
+        return ans;
+    }
+};
