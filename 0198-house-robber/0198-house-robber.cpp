27d9c2570c7b10768ffb1e8d9586eb864d39690c
Naveen Sharma <naveensharma9267@gmail.com>
2026-07-03 12:09:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (63.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,3 +1,29 @@
+
+// my approch time complexity o(N square) space complexity= o(1)
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n= nums.size();
+//         vector<int>dp(n+1,0);
+//         dp[0]=nums[0];
+//         if(n>1)
+//             dp[1]=nums[1];
+//         for(int i=2;i<n;i++){
+//             int maxi=0;
+//             for(int j=0;j<i-1;j++){
+//                 maxi=max(dp[j]+nums[i],maxi);
+//             }
+//             dp[i]=maxi;
+
+//         }
+//         if(n<=1)
+//             return dp[0];
+//         return max(dp[n-1],dp[n-2]);
+//     }
+// };
+
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -5,13 +31,11 @@ public:
         vector<int>dp(n+1,0);
         dp[0]=nums[0];
         if(n>1)
-            dp[1]=nums[1];
+            dp[1]=max(nums[1],nums[0]);
+        int maxi=0;
         for(int i=2;i<n;i++){
-            int maxi=0;
-            for(int j=0;j<i-1;j++){
-                maxi=max(dp[j]+nums[i],maxi);
-            }
-            dp[i]=maxi;
+           maxi = max(dp[i-1],dp[i-2]+nums[i]);
+           dp[i]= maxi;
 
         }
         if(n<=1)
