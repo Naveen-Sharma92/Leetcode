25f3393f72d8d7a6c340c14a6197d8d36d6feaea
Naveen Sharma <naveensharma9267@gmail.com>
2026-07-11 10:36:46 +0530

    Time: 0 ms (100%), Space: 10 MB (89.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -23,23 +23,46 @@
 //     }
 // };
 
+// O(N, o(N))
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n= nums.size();
+//         vector<int>dp(n+1,0);
+//         dp[0]=nums[0];
+//         if(n>1)
+//             dp[1]=max(nums[1],nums[0]);
+//         int maxi=0;
+//         for(int i=2;i<n;i++){
+//            maxi = max(dp[i-1],dp[i-2]+nums[i]);
+//            dp[i]= maxi;
+
+//         }
+//         if(n<=1)
+//             return dp[0];
+//         return max(dp[n-1],dp[n-2]);
+//     }
+// };
+
+
+// space optimised o(N),o(1)
 
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n= nums.size();
-        vector<int>dp(n+1,0);
-        dp[0]=nums[0];
-        if(n>1)
-            dp[1]=max(nums[1],nums[0]);
-        int maxi=0;
-        for(int i=2;i<n;i++){
-           maxi = max(dp[i-1],dp[i-2]+nums[i]);
-           dp[i]= maxi;
-
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        
+        int prev2 = nums[0]; // Represents dp[i-2]
+        int prev1 = max(nums[0], nums[1]); // Represents dp[i-1]
+        
+        for (int i = 2; i < n; i++) {
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        if(n<=1)
-            return dp[0];
-        return max(dp[n-1],dp[n-2]);
+        
+        return prev1;
     }
 };
