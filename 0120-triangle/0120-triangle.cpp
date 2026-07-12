1349926a089b712894284ed94a34a16ac9a9da9c
Naveen Sharma <naveensharma9267@gmail.com>
2026-07-12 18:08:33 +0530

    Time: 2 ms (16.61%), Space: 13.4 MB (45.09%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,41 @@
+//memoisation
+
+class Solution {
+public:
+    int dpp(int m,int n, vector<vector<int>>& dp,vector<vector<int>>& triangle){
+        if(m<0 || n<0)
+            return INT_MAX;
+        if(m==0)
+            return triangle[0][0];
+        int up= INT_MAX,upleft=INT_MAX;
+
+        if(n>0){
+            if(dp[m-1][n-1] !=-1)
+                upleft = dp[m-1][n-1]+triangle[m][n];
+            else{
+                dp[m-1][n-1] = dpp(m-1,n-1,dp,triangle);
+                upleft = dp[m-1][n-1]+triangle[m][n];
+            }
+        }
+        if(m>n){
+            if(dp[m-1][n] !=-1)
+                up = dp[m-1][n]+triangle[m][n];
+            else{
+                dp[m-1][n] = dpp(m-1,n,dp,triangle);
+                up = dp[m-1][n]+triangle[m][n];
+            }
+        }
+        return dp[m][n] = min(up,upleft);
+        
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m= triangle.size();
+        int n= triangle[m-1].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        int ans= INT_MAX;
+        for( int i= 0;i<n;i++){
+            ans= min(ans,dpp(m-1,i,dp,triangle));
+        }
+        return ans;
+    }
+};
