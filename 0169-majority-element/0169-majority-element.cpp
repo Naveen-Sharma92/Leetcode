8814fe6c53a29e8f903a798b0c73237193cad419
Naveen Sharma <naveensharma9267@gmail.com>
2026-07-13 13:42:31 +0530

    Time: 0 ms (100%), Space: 28.3 MB (10.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -32,11 +32,31 @@
 // };
 
 //revise
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//        int n= nums.size();
+//        sort(nums.begin(),nums.end());
+//        return nums[n/2];
+//     }
+// };
+
+// optimal is moore voting algorithm
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
        int n= nums.size();
-       sort(nums.begin(),nums.end());
-       return nums[n/2];
+       int candidate=nums[0],count=1;
+       for(int i=1;i<n;i++){
+        if (count==0){
+            count=1;
+            candidate=nums[i];
+        }
+        else if(candidate==nums[i])
+            count++;
+        else
+            count--;
+       }
+       return candidate;
     }
 };
