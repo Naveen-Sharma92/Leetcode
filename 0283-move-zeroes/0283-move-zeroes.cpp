e2cf9df09777c9341da76c62948c6b8d196edbd0
Naveen Sharma <naveensharma9267@gmail.com>
2026-07-13 13:29:34 +0530

    Time: 0 ms (100%), Space: 24 MB (19.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,35 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//      int k=0;
+//      int n = nums.size();
+//      for(int i=0;i<n;i++){
+//         if(nums[i]!=0){
+//             nums[k]=nums[i];
+//             k++;
+//         }
+//      }
+//      for(int i=n-1;i>=k;i--){
+//         nums[i]=0;
+//      }   
+//     }
+// };
+
+//revise
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-     int k=0;
-     int n = nums.size();
+     int n= nums.size(),k=0;
+
      for(int i=0;i<n;i++){
         if(nums[i]!=0){
             nums[k]=nums[i];
             k++;
         }
      }
-     for(int i=n-1;i>=k;i--){
-        nums[i]=0;
-     }   
+     for(;k<n;k++){
+        nums[k]=0;
+     }
+    return;
     }
 };
