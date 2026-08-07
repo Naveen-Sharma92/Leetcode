9e294ba2004e6697d2f0c531634dd0f810313679
Naveen Sharma <naveensharma9267@gmail.com>
2026-08-07 19:06:15 +0530

    Time: 4 ms (20.98%), Space: 40.1 MB (81.78%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,16 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n= nums.size();
+//         vector<int>prefix(n,1),suffix(n,1),ans(n);
+//         for(int i=1;i<n;i++){
+//             prefix[i]=prefix[i-1]*nums[i-1];
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             suffix[i]= suffix[i+1] * nums[i+1];
+//         }
+//         for(int i=0;i<n;i++)
+//             ans[i]=prefix[i]*suffix[i];
+//         return ans;
+//     }
+// };
+ // optimal space
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n= nums.size();
-        vector<int>prefix(n,1),suffix(n,1),ans(n);
+        vector<int>prefix(n,1);
+        int suffix=1;
         for(int i=1;i<n;i++){
             prefix[i]=prefix[i-1]*nums[i-1];
         }
         for(int i=n-2;i>=0;i--){
-            suffix[i]= suffix[i+1] * nums[i+1];
+            suffix= suffix*nums[i+1];
+            prefix[i]=prefix[i]*suffix;
         }
-        for(int i=0;i<n;i++)
-            ans[i]=prefix[i]*suffix[i];
-        return ans;
+        return prefix;
     }
 };
