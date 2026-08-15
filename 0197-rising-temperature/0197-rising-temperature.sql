dff8cb12509a08cd5fd23dd16642c7749dd44f21
Naveen Sharma <naveensharma9267@gmail.com>
2026-08-15 23:54:33 +0530

    Time: 637 ms (13.54%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
-select p.id from Weather as p cross join Weather as y
+#select p.id from Weather as p cross join Weather as y where datediff(p.recorddate,y.recorddate)=1 AND p.temperature >y.temperature
+
+#another way
+
+select p.id from weather p,weather y
 where datediff(p.recorddate,y.recorddate)=1 AND p.temperature >y.temperature
