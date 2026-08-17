be805a80574f58aec4888d076e4732119e1256d9
Naveen Sharma <naveensharma9267@gmail.com>
2026-08-17 22:07:34 +0530

    Time: 0 ms (100%), Space: 9.1 MB (12.8%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,18 +1,40 @@
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+//         string ans;
+//         stack<char>st;
+//         int c=0;
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]=='('){
+//                 st.push('(');
+//                 if(c!=0)
+//                     ans+='(';
+//                 c++;
+//             }
+//             else if(s[i]==')'){
+//                 st.pop();
+//                 c--;
+//                 if(c!=0)
+//                     ans+=')';
+//             }
+//         }
+//         return ans;
+//     }
+// };
+//space optimised we not need stack just remove it
+
 class Solution {
 public:
     string removeOuterParentheses(string s) {
         string ans;
-        stack<char>st;
         int c=0;
         for(int i=0;i<s.size();i++){
             if(s[i]=='('){
-                st.push('(');
                 if(c!=0)
                     ans+='(';
                 c++;
             }
             else if(s[i]==')'){
-                st.pop();
                 c--;
                 if(c!=0)
                     ans+=')';
