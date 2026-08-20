5bf533dc72025ffeb4a98a9dd3f2d2f0e15f49af
Naveen Sharma <naveensharma9267@gmail.com>
2026-08-20 11:49:58 +0530

    Time: 8 ms (66.04%), Space: 30.7 MB (86.26%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,26 +1,60 @@
+//tabulation
+
+// class Solution {
+// public:
+//     int maximalSquare(vector<vector<char>>& matrix) {
+//         int m= matrix.size();
+//         int n=matrix[0].size();
+//         vector<vector<int>>dp(m,vector<int>(n,0));
+//         int maxi=0;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(matrix[i][j]=='0')
+//                     continue;
+//                 else{
+//                     int upleft=0,up=0,left=0;
+//                     if(j-1>=0 && i-1>=0)
+//                         upleft=dp[i-1][j-1];
+//                     if(j-1>=0)
+//                         left=dp[i][j-1];
+//                     if(i-1>=0)
+//                         up=dp[i-1][j];
+//                     dp[i][j]=min(upleft,min(up,left))+1;
+//                     maxi=max(maxi,dp[i][j]);
+//             }
+//             }
+//         }
+//         return maxi*maxi;  //as maxi is side length
+//     }
+// };
+
+//space optimisation
+
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m= matrix.size();
         int n=matrix[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));
+        vector<int>prev(n,0);
         int maxi=0;
         for(int i=0;i<m;i++){
+            vector<int>curr(n,0);
             for(int j=0;j<n;j++){
                 if(matrix[i][j]=='0')
                     continue;
                 else{
                     int upleft=0,up=0,left=0;
                     if(j-1>=0 && i-1>=0)
-                        upleft=dp[i-1][j-1];
+                        upleft=prev[j-1];
                     if(j-1>=0)
-                        left=dp[i][j-1];
+                        left=curr[j-1];
                     if(i-1>=0)
-                        up=dp[i-1][j];
-                    dp[i][j]=min(upleft,min(up,left))+1;
-                    maxi=max(maxi,dp[i][j]);
+                        up=prev[j];
+                    curr[j]=min(upleft,min(up,left))+1;
+                    maxi=max(maxi,curr[j]);
             }
             }
+            prev=curr;
         }
         return maxi*maxi;  //as maxi is side length
     }
