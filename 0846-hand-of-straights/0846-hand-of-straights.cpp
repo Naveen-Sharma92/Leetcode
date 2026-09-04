1cf9bcd64ec5b0b2e40410dabb92f5d59e5ba55e
Naveen Sharma <naveensharma9267@gmail.com>
2026-09-04 19:32:44 +0530

    Time: 43 ms (35.28%), Space: 32.4 MB (22.53%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n=hand.size();
+        map<int,int>mpp;
+        for(int i=0;i<n;i++)
+            mpp[hand[i]]++;
+
+        while(mpp.size()){
+            int start=mpp.begin()->first;
+            for(int i=0;i<groupSize;i++){
+                if(mpp[start+i]<=0)
+                    return false;
+                mpp[start+i]--;
+                if(mpp[start+i] == 0)
+                    mpp.erase(start+i);
+            }
+        }
+        return true;
+        
+    }
+};
