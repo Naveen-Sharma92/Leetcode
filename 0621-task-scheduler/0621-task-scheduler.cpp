195f9ca39aef8fcedc33d53927686007d242ab87
Naveen Sharma <naveensharma9267@gmail.com>
2026-09-04 12:32:56 +0530

    Time: 20 ms (49.38%), Space: 42.9 MB (51.61%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,51 +1,101 @@
+//approch is right and need to read in next i just remove sorting to takedown sorting time complexity using vector
+// class Solution {
+// public:
+//     int leastInterval(vector<char>& tasks, int n) {
+//         int c=1;
+//         int s= tasks.size();
+//         sort(tasks.begin(),tasks.end());
+//         priority_queue<pair<int,char>>pq; //max heap of freq,char 1st queue
+//         for(int i=1;i<s;i++){
+//             if(tasks[i-1]!=tasks[i]){
+//                 pq.push({c,tasks[i-1]});
+//                 c=1;
+//                 continue;
+//             }
+//             c++;
+//         }
+//         pq.push({c,tasks[s-1]});
+
+//         queue<pair<pair<int,char>,int>>q; // cooldown 2nd q where we store freq,task,time it will be available;
+//         int anstime=0;
+//         while((!pq.empty()) || (!q.empty())){
+//             anstime++;
+//             // below is right and pass but a bug as i need to add all that is available to pq otherswise the most frequent one does not add to pq
+//             // if(!q.empty()){
+//             //     int f=q.front().first.first;
+//             //     char ch=q.front().first.second;
+//             //     int t=q.front().second;
+//             //     if(t<=anstime){ // check cooldown is over <= not == as at same time more can be cooldown but can call one only
+//             //         pq.push({f,ch});
+//             //         q.pop();
+//             //     }
+//             // }
+//             while(!q.empty() && q.front().second <= anstime){// check cooldown is over <= not == as at same time more can be cooldown but can call one only
+//                 pq.push(q.front().first);
+//                     q.pop();
+//             }
+
+//             if(!pq.empty()){
+//                 int f= pq.top().first;
+//                 int ch=pq.top().second;
+//                 pq.pop();
+//                 f--;
+//                 if(f>0){
+//                     q.push({{f,ch},anstime+n+1});
+//                 }
+//             }
+
+//         }
+//         return anstime;
+        
+//     }
+// };
+
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int c=1;
-        int s= tasks.size();
-        sort(tasks.begin(),tasks.end());
-        priority_queue<pair<int,char>>pq; //max heap of freq,char 1st queue
-        for(int i=1;i<s;i++){
-            if(tasks[i-1]!=tasks[i]){
-                pq.push({c,tasks[i-1]});
-                c=1;
-                continue;
-            }
-            c++;
+        
+        // count frequency of each task
+        vector<int> freq(26, 0);
+        for(auto ch : tasks)
+            freq[ch-'A']++;
+
+        // max heap: highest frequency first
+        priority_queue<int> pq;
+
+        for(auto f : freq){
+            if(f > 0)
+                pq.push(f);
         }
-        pq.push({c,tasks[s-1]});
 
-        queue<pair<pair<int,char>,int>>q; // cooldown 2nd q where we store freq,task,time it will be available;
-        int anstime=0;
-        while((!pq.empty()) || (!q.empty())){
+        // {{remaining frequency}, available time}
+        queue<pair<int,int>> q;
+
+        int anstime = 0;
+
+        while(!pq.empty() || !q.empty()){
+
             anstime++;
-            // below is right and pass but a bug as i need to add all that is available to pq otherswise the most frequent one does not add to pq
-            // if(!q.empty()){
-            //     int f=q.front().first.first;
-            //     char ch=q.front().first.second;
-            //     int t=q.front().second;
-            //     if(t<=anstime){ // check cooldown is over <= not == as at same time more can be cooldown but can call one only
-            //         pq.push({f,ch});
-            //         q.pop();
-            //     }
-            // }
-            while(!q.empty() && q.front().second <= anstime){// check cooldown is over <= not == as at same time more can be cooldown but can call one only
+
+            // add ALL tasks whose cooldown is over
+            while(!q.empty() && q.front().second <= anstime){
                 pq.push(q.front().first);
-                    q.pop();
+                q.pop();
             }
 
+            // execute highest frequency available task
             if(!pq.empty()){
-                int f= pq.top().first;
-                int ch=pq.top().second;
+                int f = pq.top();
                 pq.pop();
+
                 f--;
-                if(f>0){
-                    q.push({{f,ch},anstime+n+1});
+
+                if(f > 0){
+                    q.push({f, anstime+n+1});
                 }
             }
-
         }
+
         return anstime;
-        
     }
 };
