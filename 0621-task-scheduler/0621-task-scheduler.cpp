1a8d9215c72ea00d3b5d2c7249fd0a59694eb2d0
Naveen Sharma <naveensharma9267@gmail.com>
2026-09-04 12:28:19 +0530

    Time: 21 ms (48.14%), Space: 44.7 MB (43.22%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int c=1;
+        int s= tasks.size();
+        sort(tasks.begin(),tasks.end());
+        priority_queue<pair<int,char>>pq; //max heap of freq,char 1st queue
+        for(int i=1;i<s;i++){
+            if(tasks[i-1]!=tasks[i]){
+                pq.push({c,tasks[i-1]});
+                c=1;
+                continue;
+            }
+            c++;
+        }
+        pq.push({c,tasks[s-1]});
+
+        queue<pair<pair<int,char>,int>>q; // cooldown 2nd q where we store freq,task,time it will be available;
+        int anstime=0;
+        while((!pq.empty()) || (!q.empty())){
+            anstime++;
+            if(!q.empty()){
+                int f=q.front().first.first;
+                char ch=q.front().first.second;
+                int t=q.front().second;
+                if(t<=anstime){ // check cooldown is over <= not == as at same time more can be cooldown but can call one only
+                    pq.push({f,ch});
+                    q.pop();
+                }
+            }
+            if(!pq.empty()){
+                int f= pq.top().first;
+                int ch=pq.top().second;
+                pq.pop();
+                f--;
+                if(f>0){
+                    q.push({{f,ch},anstime+n+1});
+                }
+            }
+
+        }
+        return anstime;
+        
+    }
+};
